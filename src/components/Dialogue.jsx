33585529f6f0589d71b9047b13017c28b4e510e1
drewhsu86 <bowserhsu@gmail.com>
2020-08-12 15:30:59 -0400

    text array and button works for dialogue scene

--- src/components/Dialogue.jsx
+++ src/components/Dialogue.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Dialogue() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function Dialogue(props) {
+  const { scene, assets, changeScene } = props.controller
+
+  // index of text array from scene 
+  const [currText, setCurrText] = useState(0)
+
+  console.log('currText', currText)
+
+  if (!scene) {
+    return <h1>No Valid Dialogue Scene Loaded</h1>
+  } else {
+    // set css styles for scene 
+    const styles = {
+      backgroundImage: `url("${assets[scene.background]}")`
+    }
+
+    const handleMessage = () => {
+      if (currText < scene.text.length) {
+        setCurrText(currText+1)
+      }
+    }
+
+    return (
+      <div className="screen"
+        style={styles}
+      >
+  
+        <div className="dialogue-box" onClick={handleMessage}>
+          {/* if array is finished, ask if move on to next scene  */}
+          {
+            currText < scene.text.length ? <p>
+              { scene.text[currText] }
+            </p> : <div className="dialogue-horizontal">
+                <button onClick={() => changeScene(scene.gotoScene)}>
+                  Go To Next Scene
+                </button>
+                <button onClick={() => setCurrText(0)}>
+                  Watch Scene Again
+                </button>
+                </div>
+          }
+        </div>
+      </div>
+    )
+  }
 }
--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import Title from './Title'
+import Dialogue from './Dialogue'
 
 export default function Scene(props) {
   const { scene } = props.controller
 
   switch (scene.type) {
     case 'title':
-      return (
-        <Title controller={props.controller} />
-      )
+      return <Title controller={props.controller} />
+    case 'dialogue':
+      return <Dialogue controller={props.controller} />
     default: 
-      return (
-        <h1>No Defined Scene Type In JSON</h1>
-      )
+      return <h1>No Defined Scene Type In JSON</h1>
+
   }
 }
